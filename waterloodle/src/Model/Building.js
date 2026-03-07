61d92557610e8262f0980284a42da4f228131e50
Mya Kay <m2kay@uwaterloo.ca>
2026-03-07 13:05:54 -0500

    Added Building classs

---
+++ waterloodle/src/Model/Building.js
@@ -0,0 +1,13 @@
+/*
+Data structure to store information about a building
+*/
+
+export class Building {
+    name;
+    faculty; // faculty name, or NA
+    numFloors;
+    builtYear;
+    servesFood; // T/F
+    connectsTo; // Array of buildings it connects to via bridges/tunnels
+    blurb;
+}
